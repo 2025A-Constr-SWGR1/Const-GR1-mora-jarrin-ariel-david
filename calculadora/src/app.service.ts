08387ace3ceb65382b4641e50d61080c52e88c09
Aidmora <arielabc389@gmail.com>
2025-04-30 15:31:14 -0500

    Rules, Build, Lint and Test

--- calculadora/src/app.service.ts
+++ calculadora/src/app.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    // console.log('Log');
+    // console.debug('Debug');
+    // console.info('Info');
+    console.warn('Warn');
+    console.error('Error');
+    return 'Hello World!!';
   }
 }
