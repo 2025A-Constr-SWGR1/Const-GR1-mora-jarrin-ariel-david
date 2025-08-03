4096f95fa44c169d91ef66ed4453c23817fb3b2f
Aidmora <arielabc389@gmail.com>
2025-08-03 10:44:14 -0500

    Examen Final

---
+++ calculadora-principal-examen/index.js
@@ -0,0 +1,9 @@
+const suma = require('2025a-swgr1-mjad-suma');
+const resta = require('2025a-swgr1-mjad-resta');
+const multiplicacion = require('2025a-swgr1-mjad-multiplicacion');
+const division = require('2025a-swgr1-mjad-division');
+
+console.log('suma(2,1):', suma.suma(2,1)); 
+console.log('resta(3,2):', resta.resta(3,2)); 
+console.log('multiplicacion(2,5):', multiplicacion.multiplicacion(2,5)); 
+console.log('division(9,3):', division.division(9,3)); 
